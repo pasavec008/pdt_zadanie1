4240d9ef20335cdb21cc1304d98528e3c33f7b2a
pasavec008 <m.adamovic999@gmail.com>
2022-09-30 21:08:18 +0200

    add constraints

---
+++ add_constraints.py
@@ -0,0 +1,22 @@
+def add_constraints(conn):
+    cursor = conn.cursor()
+    cursor.execute('''
+        ALTER TABLE conversations
+        ADD CONSTRAINT fk_user FOREIGN KEY(author_id) REFERENCES authors(id);
+
+        ALTER TABLE hashtags
+        ADD CONSTRAINT unq_key UNIQUE(tag);
+
+        ALTER TABLE conversation_hashtags
+        ADD CONSTRAINT fk_conversation FOREIGN KEY(conversation_id) REFERENCES conversations(id);
+        ALTER TABLE conversation_hashtags
+        ADD CONSTRAINT fk_hashtag FOREIGN KEY(hashtag_id) REFERENCES hashtags(id);
+
+        ALTER TABLE context_annotations
+        ADD CONSTRAINT fk_conversation FOREIGN KEY(conversation_id) REFERENCES conversations(id);
+        ALTER TABLE context_annotations
+        ADD CONSTRAINT fk_context_domain FOREIGN KEY(context_domain_id) REFERENCES context_domains(id);
+        ALTER TABLE context_annotations
+        ADD CONSTRAINT fk_context_entity FOREIGN KEY(context_entity_id) REFERENCES context_entities(id);
+    ''')
+    conn.commit()
--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import gzip
 import database_init
 import main_migration_authors
 import main_migration_conversations
+import add_constraints
 
 def main():
     conn = database_init.connect_to_database()
     database_init.create_tables(conn)
 
-    authors_file = gzip.open('data/authors.jsonl.gz')
-    authors_hash_map = main_migration_authors.migration(conn, authors_file)
+    # authors_file = gzip.open('data/authors.jsonl.gz')
+    # authors_hash_map = main_migration_authors.migration(conn, authors_file)
 
-    conversations_file = gzip.open('data/conversations.jsonl.gz')
-    main_migration_conversations.migration(conn, conversations_file, authors_hash_map)
+    # conversations_file = gzip.open('data/conversations.jsonl.gz')
+    # main_migration_conversations.migration(conn, conversations_file, authors_hash_map)
 
+    add_constraints.add_constraints(conn)
     conn.close()
     return
 
