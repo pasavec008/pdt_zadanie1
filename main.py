852853c30604f5239ef4e9d0118afebc25524a3a
pasavec008 <m.adamovic999@gmail.com>
2022-10-01 15:31:59 +0200

    Log fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     conn = database_init.connect_to_database()
     print("Connected to database")
     database_init.create_tables(conn)
-    print("Database and tables setup completed in time: ", print(time.time()-start))
+    print("Database and tables setup completed in time: ", time.time()-start)
 
     #first reading (author file)
     start = time.time()
@@ -22,14 +22,14 @@ def main():
     authors_file = gzip.open('data/authors.jsonl.gz')
     authors_hashmap = main_migration_authors.migration(conn, authors_file)
     authors_file.close()
-    print("First reading completed in time: ", print(time.time()-start))
+    print("First reading completed in time: ", time.time()-start)
 
     #second reading (conv file)
     start = time.time()
     print("Second reading (conversations file)")
     conversations_file = gzip.open('data/conversations.jsonl.gz')
     conversations_hashmap = main_migration_conversations.migration(conn, conversations_file, authors_hashmap)
-    print("Second reading completed in time: ", print(time.time()-start))
+    print("Second reading completed in time: ", time.time()-start)
 
     #third reading (conv file)
     start = time.time()
@@ -37,11 +37,11 @@ def main():
     conversations_file.seek(0)
     main_migration_references.migration(conn, conversations_file, conversations_hashmap)
     conversations_file.close()
-    print("Third reading completed in time: ", print(time.time()-start))
+    print("Third reading completed in time: ", time.time()-start)
 
     print("Adding constraints")
     add_constraints.add_constraints(conn)
-    print("Constraints added in time: ", print(time.time()-start))
+    print("Constraints added in time: ", time.time()-start)
     
     conn.close()
     return
